950d45acedb41519bd3208ebcd9c8a334bf934e9
Koichi <Tsukishima_Koichi@outlook.com>
2024-07-13 23:03:53 +0800

    0.template

---
+++ STM32/STM32_Project/0.template/Objects/core_cm3.d
@@ -0,0 +1,1 @@
+./objects/core_cm3.o: Start\core_cm3.c
---
+++ STM32/STM32_Project/0.template/Objects/misc.d
@@ -0,0 +1,14 @@
+./objects/misc.o: Library\src\misc.c Library\inc\misc.h Start\stm32f10x.h \
+  Start\Include\core_cm3.h Start\system_stm32f10x.h \
+  User\stm32f10x_conf.h Library\inc\stm32f10x_adc.h \
+  Library\inc\stm32f10x_bkp.h Library\inc\stm32f10x_can.h \
+  Library\inc\stm32f10x_cec.h Library\inc\stm32f10x_crc.h \
+  Library\inc\stm32f10x_dac.h Library\inc\stm32f10x_dbgmcu.h \
+  Library\inc\stm32f10x_dma.h Library\inc\stm32f10x_exti.h \
+  Library\inc\stm32f10x_flash.h Library\inc\stm32f10x_fsmc.h \
+  Library\inc\stm32f10x_gpio.h Library\inc\stm32f10x_i2c.h \
+  Library\inc\stm32f10x_iwdg.h Library\inc\stm32f10x_pwr.h \
+  Library\inc\stm32f10x_rcc.h Library\inc\stm32f10x_rtc.h \
+  Library\inc\stm32f10x_sdio.h Library\inc\stm32f10x_spi.h \
+  Library\inc\stm32f10x_tim.h Library\inc\stm32f10x_usart.h \
+  Library\inc\stm32f10x_wwdg.h
